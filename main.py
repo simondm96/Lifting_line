1beda0adc21b46e8b88482b2e06ee8c3194a817f
Simon De Meester <simon.de.meester@telenet.be>
2018-04-25 15:56:58 +0200

    Added induced velocity function

--- main.py
+++ main.py
@@ -6,3 +6,29 @@ Created on Wed Apr 25 14:41:32 2018
 @author: Simon, Zyanya, Wessel
 """
 
+import numpy as np
+
+def induced_velocity(point, point_1, point_2, circulation, r_vortex = 1e-10):
+    """
+    Calclates the induced velocity of a straight vortex element between point_1 and point_2 on point
+    """
+    point = np.asarray(point)
+    point_1 = np.asarray(point_1)
+    point_2 = np.asarray(point_2)
+    
+    r1 = point_1-point
+    r2 = point_2-point
+    r0 = r1-r2
+    
+    cross_r = np.cross(r1, r2)
+    cross_squared = np.linalg.norm(cross_r)**2
+    r1_norm = np.linalg.norm(r1)
+    r2_norm = np.linalg.norm(r2)
+    if r1_norm<r_vortex or r2_norm<r_vortex or cross_squared<r_vortex:
+        v_ind = np.array([0,0,0])
+    else:
+        vector_1 = circulation/(4*np.pi)*cross_r/cross_squared
+        vector_2 = np.inner(r0, r1/r1_norm-r2/r2_norm)
+        v_ind = vector_1*vector_2
+    return v_ind
+    
