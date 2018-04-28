c35f561834910f21c0d5c1c94d13993219cc1ad3
wesselkeijer <w.j.t.keijer@student.tudelft.nl>
2018-04-28 13:14:20 +0200

    Added rotor coordinate matrix

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ Created on Wed Apr 25 14:41:32 2018
 
 import numpy as np
 import csv
+import matplotlib.pyplot as plt
 
 
 """
@@ -21,6 +22,41 @@ pitch = 2*np.pi/180
 a = 0.3 #starting value
 aprime = 0.0 #starting value
 rho = 1.225
+
+N = 100
+
+
+"""
+Initialize coordlist matrix of rotor coordinates
+"""
+coordlist = np.zeros((1,3))
+
+
+for tn in range(3):
+    theta = 90+tn*120
+    print theta
+    r=0
+    for N_el in range(N):
+        dr = R/N
+        r = r+dr
+        x = r*np.cos(np.radians(theta))
+        y = r*np.sin(np.radians(theta))
+        z = 0
+        tempmat = [[x,y,z]]
+        coordlist = np.vstack([coordlist, tempmat])
+
+#xlist = []
+#ylist = []
+#for k in range(N*3):
+#    x = coordlist[k][0]
+#    y = coordlist[k][1]
+#    xlist.append(x)
+#    ylist.append(y)
+#    
+#plt.plot(ylist, xlist, 'o')
+#plt.axis('equal')
+#plt.show()
+
 
 def induced_velocity(point, point_1, point_2, circulation, r_vortex = 1e-10):
     """
@@ -85,7 +121,7 @@ Very basic circulation calculator with lots of inputs and 1 output
 def circcalc(alpha, V, rho, c):
     circ = 0.5*c*V*polarreader(alpha, alphalist, cllist, cdlist)[0]
     return circ
-   
+  
      
 """
 Initialize u, v, w matrices with circulation/ring strength set to unity
@@ -93,12 +129,12 @@ Initialize u, v, w matrices with circulation/ring strength set to unity
 N = 100
 controlpoints = np.zeros(N)  
   
-MatrixU = np.zeros(N, N)
-MatrixV = np.zeros(N, N)
-MatrixW = np.zeros(N, N)
+#MatrixU = np.zeros((N, N))
+#MatrixV = np.zeros((N, N))
+#MatrixW = np.zeros((N, N))
 #for icp in range(len(controlpoints)):
 #    for jring in range(len(controlpoints)):
-#        MatrixU[icp][jring] = induced_velocity(
-#        MatrixV[icp][jring] = induced_velocity(
-#        MatrixW[icp][jring] = induced_velocity(
+#        MatrixU[icp][jring] = unit_induced_velocity(controlpoint[i], controlpoints)
+#        MatrixV[icp][jring] = unit_induced_velocity(controlpoint[i], controlpoints)
+#        MatrixW[icp][jring] = unit_induced_velocity(controlpoint[i], controlpoints)
         
